b095ddfa4826d586351ec28014dec343d0046a6a
nikhil-2605 <nikhilsinha101@gmail.com>
2021-04-10 13:15:26 +0530

    version 2 : without getBeab()

--- src/main/java/com/example/TicketBookingApplication.java
+++ src/main/java/com/example/TicketBookingApplication.java
@@ -1,6 +1,11 @@
 package com.example;
 
 import java.util.Date;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -8,19 +13,28 @@ import com.example.entities.Ticket;
 import com.example.service.TicketBookingService;
 
 @SpringBootApplication
-public class TicketBookingApplication {
-	public static void main(String[] args) {
-	ConfigurableApplicationContext applicationContext = SpringApplication.run(TicketBookingApplication.class, args);
-	//  TicketBookingService ticketBookingService = applicationContext.getBean("ticketBookingService", TicketBookingService.class);
-		  
-	TicketBookingService ticketBookingService = applicationContext.getBean(TicketBookingService.class);
+public class TicketBookingApplication implements CommandLineRunner{
+	@Autowired  
+	TicketBookingService ticketBookingService;
 	
-	Ticket t1=new Ticket(); t1.setBookingDate(new Date());
-	  t1.setDestination("blore"); t1.setSource("bhilai");
-	  t1.setPassengerName("nik"); t1.setEmail("niksin@gmail.com");
-	   
-	  ticketBookingService.createTicket(t1);
+		@Autowired
+		private DataSource datasource;
+	public static void main(String[] args) {
+	SpringApplication.run(TicketBookingApplication.class, args);
+	//  TicketBookingService ticketBookingService = applicationContext.getBean("ticketBookingService", TicketBookingService.class);
 	  
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Ticket t1=new Ticket(); t1.setBookingDate(new Date());
+		  t1.setDestination("blore"); t1.setSource("bhilai");
+		  t1.setPassengerName("nik"); t1.setEmail("niksin@gmail.com");
+		   
+		  ticketBookingService.createTicket(t1);
+		  
+		  System.out.println("Datasource ->"+datasource);
+		
 	}
 
 }
